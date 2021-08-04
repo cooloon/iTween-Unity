46ea10977e5b13d922a2acfcb96b3951f73a0ad7
Shin Takano <cooloon@gmail.com>
2021-08-04 11:32:06 +0900

    Enable drawing a path with Handles

--- Runtime/iTween.cs
+++ Runtime/iTween.cs
@@ -6334,8 +6334,8 @@ public class iTween : MonoBehaviour
 			if(method == "gizmos"){
 				Gizmos.DrawLine(line[i], line[i+1]);;
 			}else if(method == "handles"){
-				Debug.LogError("iTween Error: Drawing a line with Handles is temporarily disabled because of compatability issues with Unity 2.6!");
-				//UnityEditor.Handles.DrawLine(line[i], line[i+1]);
+				//Debug.LogError("iTween Error: Drawing a line with Handles is temporarily disabled because of compatability issues with Unity 2.6!");
+				UnityEditor.Handles.DrawLine(line[i], line[i+1]);
 			}
 		}
 	}		
@@ -6353,8 +6353,8 @@ public class iTween : MonoBehaviour
 			if(method == "gizmos"){
 				Gizmos.DrawLine(currPt, prevPt);
 			}else if(method == "handles"){
-				Debug.LogError("iTween Error: Drawing a path with Handles is temporarily disabled because of compatability issues with Unity 2.6!");
-				//UnityEditor.Handles.DrawLine(currPt, prevPt);
+				//Debug.LogError("iTween Error: Drawing a path with Handles is temporarily disabled because of compatability issues with Unity 2.6!");
+				UnityEditor.Handles.DrawLine(currPt, prevPt);
 			}
 			prevPt = currPt;
 		}
